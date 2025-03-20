9d5aaa07866797c27c7a5af978f2ec7751bbd2cf
firyalshafa <shafafiryal782@gmail.com>
2025-03-20 21:38:14 +0800

    Menambahkan string koneksi ke database SQL Server. untuk menghubungkan aplikasi dengan database OrganisasiMahasiswa di SQL Server yang berjalan pada LAPTOP-SOF8NSPF\\FIRYAL, menggunakan Integrated Security untuk autentikasi Windows.

--- pertemuan3/Form1.cs
+++ pertemuan3/Form1.cs
@@ -12,9 +12,22 @@ namespace pertemuan3
 {
     public partial class Form1 : Form
     {
+        // Ganti "SERVER" sesuai dengan SQL Server Anda
+        private string connectionString = "Data Source=LAPTOP-SOF8NSPF\\FIRYAL;Initial Catalog=OrganisasiMahasiswa;Integrated Security=True";
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+
         }
     }
 }
